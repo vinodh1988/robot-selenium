4d5a9a9962fb8d03d714edd3ec4609c155c86ed9
vinodh1988 <k.c.vinodh@hotmail.com>
2024-07-11 20:30:01 +0530

    control

--- first.py
+++ first.py
@@ -4,4 +4,6 @@ from pywinauto import Desktop
 Popen('control.exe', shell=True)
 dlg = Desktop(backend="uia").ControlPanel
 dlg.wait('visible')
-dlg.print_control_identifiers()
+#dlg.print_control_identifiers()
+system_security=dlg.child_window(title="System and Security", auto_id="name", control_type="Hyperlink")
+system_security.click_input()
