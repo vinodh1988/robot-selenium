455357fc75072c7503b2c4aa62e717e7a2b3623c
vinodh1988 <k.c.vinodh@hotmail.com>
2024-07-12 20:13:44 +0530

    mousemovement

--- capture.py
+++ capture.py
@@ -4,4 +4,11 @@ import time
 pyautogui.press("win")
 icon=pyautogui.locateOnScreen('photos.png',confidence=0.9)
 print(icon)
+x,y=icon.left,icon.top
+iconnow=pyautogui.center(icon)
+x,y=iconnow
+print(x,y)
+pyautogui.moveTo(x,y,duration=4)
+pyautogui.click()
+print(iconnow)
 time.sleep(15)
